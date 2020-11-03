b243ecbb9c0fddac25840221ffe64d120831db57
hooks7 <1791131967@qq.com>
2020-11-03 19:10:02 +0800

    nav

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import 'antd-mobile/dist/antd-mobile.css';
 import './assets/fonts/iconfont.css';
+
 import './index.css';
 import App from './App';
 
--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
-import { Carousel } from 'antd-mobile';
+import { Carousel, Flex, WhiteSpace } from 'antd-mobile';
+
+import './index.scss';
 
 import axios from 'axios';
+
+import nav1 from '../../assets/images/nav-1.png';
+import nav2 from '../../assets/images/nav-2.png';
+import nav3 from '../../assets/images/nav-3.png';
+import nav4 from '../../assets/images/nav-4.png';
+
+// 导航菜单的数据
+const menus = [
+	{ name: '整租', imgSrc: nav1, path: '/home/houselist' },
+	{ name: '合租', imgSrc: nav2, path: '/home/houselist' },
+	{ name: '地图找房', imgSrc: nav3, path: '/map' },
+	{ name: '去出租', imgSrc: nav4, path: '/rent/add' }
+];
 
 export default class Index extends React.Component {
 	state = {
@@ -17,18 +32,18 @@ export default class Index extends React.Component {
 		});
 	}
 
+	// 轮播图渲染
 	renderSwiper() {
 		return this.state.swiper.map((val) => (
 			<a
 				key={val.imgSrc}
-				href="http://www.alipay.com"
+				// href="http://www.alipay.com"
 				style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
 			>
 				<img
 					src={`http://localhost:8080${val.imgSrc}`}
 					style={{ width: '100%', verticalAlign: 'top' }}
 					onLoad={() => {
-						// fire window resize event to change height
 						window.dispatchEvent(new Event('resize'));
 						this.setState({ imgHeight: 'auto' });
 					}}
@@ -37,16 +52,31 @@ export default class Index extends React.Component {
 		));
 	}
 
+	renderNavMenu() {
+		return menus.map((item) => (
+			<Flex.Item key={item.name}
+				onClick={() => {
+					this.props.history.push(item.path);
+				}}
+			>
+				<Flex direction="column">
+					<img src={item.imgSrc} style={{ width: '48px' }} />
+					<span>{item.name}</span>
+				</Flex>
+			</Flex.Item>
+		));
+	}
+
 	render() {
 		return (
-			<Carousel
-				autoplay={this.state.autoplay}
-				infinite
-				beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
-				afterChange={(index) => console.log('slide to', index)}
-			>
-				{this.renderSwiper()}
-			</Carousel>
+			<div>
+				<Carousel autoplay={this.state.autoplay} infinite>
+					{this.renderSwiper()}
+				</Carousel>
+				<div className="NavMenu">
+					<Flex>{this.renderNavMenu()}</Flex>
+				</div>
+			</div>
 		);
 	}
 }
